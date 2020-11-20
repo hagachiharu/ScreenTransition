058475425ca641f91ae2338a6b523dc974c33e68
s20b711 <s20b711@shohoku.ac.jp>
2020-11-20 10:22:45 +0900

    SubとTransを作りました。

---
+++ app/src/main/java/jp/ac/shohoku/screentransition/TransActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.shohoku.screentransition;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TransActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
