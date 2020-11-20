22f7a95fa207efcca3f01682ef754d20275d5161
RIKA07 <72699679+RIKA07@users.noreply.github.com>
2020-11-20 11:42:44 +0900

    SubActivityとTansActivity

--- app/src/main/java/jp/ac/shohoku/screentransition/TransActivity.java
+++ app/src/main/java/jp/ac/shohoku/screentransition/TransActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.shohoku.screentransition;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +12,15 @@ public class TransActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        setContentView(R.layout.activity_trans);
+        //ボタンを押したときにイベントを取得できるようにする
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                //インテントに、この画面と、遷移する別の画面を指定する
+                Intent intent = new Intent(TransActivity.this, SubActivity.class);
+                //インテントで指定した別の画面に遷移する
+                startActivity(intent);
+        }
+    });
+}}
